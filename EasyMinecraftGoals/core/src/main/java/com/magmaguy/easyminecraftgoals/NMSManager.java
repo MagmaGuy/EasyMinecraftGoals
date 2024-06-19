0bb917b4b660bd1a108c253e8bee85435347aa80
BuildTools <unconfigured@null.spigotmc.org>
2024-06-19 14:08:54 +0100

    - Updated to Minecraft 1.21

--- EasyMinecraftGoals/core/src/main/java/com/magmaguy/easyminecraftgoals/NMSManager.java
+++ EasyMinecraftGoals/core/src/main/java/com/magmaguy/easyminecraftgoals/NMSManager.java
@@ -16,24 +16,34 @@ public class NMSManager {
 
     public static void initializeAdapter(Plugin plugin) {
         pluginProvider = plugin;
-        plugin.getLogger().info(Bukkit.getServer().getClass().getPackage().getName());
+//        plugin.getLogger().info(Bukkit.getServer().getClass().getPackage().getName());
         String version = getServerVersion();
-        if (version == null) {return;}
+        if (version == null) {
+            plugin.getLogger().warning("Server version is null.");
+            return;
+        }
 
         try {
-            plugin.getLogger().info("Format: " + PACKAGE + version + ".NMSAdapter");
+//            plugin.getLogger().info("Format: " + PACKAGE + version + ".NMSAdapter");
             String versionName;
             //1.20.0 is fundamentally the same as 1.20.1 so we use R2
             if (Objects.equals(version, "v1_20_R0")) versionName = PACKAGE + "v1_20_R1" + ".NMSAdapter";
             else versionName = PACKAGE + version + ".NMSAdapter";
+//            plugin.getLogger().info("Loading class: " + versionName);
             adapter = (NMSAdapter) Class.forName(versionName).getDeclaredConstructor().newInstance();
             plugin.getLogger().log(Level.INFO, "Supported server version detected: {0}", version);
             isEnabled = true;
+        } catch (ClassNotFoundException e) {
+            plugin.getLogger().log(Level.SEVERE, "Class not found: {0}", e.getMessage());
         } catch (ReflectiveOperationException e) {
-            plugin.getLogger().log(Level.SEVERE, "Server version \"{0}\" is unsupported! Please check for updates!",
-                    version);
-            //todo: remains to be seen Bukkit.getPluginManager().disablePlugin(plugin);
-            isEnabled = false;
+            plugin.getLogger().log(Level.SEVERE, "Error instantiating class: {0}", e.getMessage());
+        } catch (Exception e) {
+            plugin.getLogger().log(Level.SEVERE, "Unexpected error: {0}", e.getMessage());
+        } finally {
+            if (!isEnabled) {
+                plugin.getLogger().log(Level.SEVERE, "Server version \"{0}\" is unsupported! Please check for updates!", version);
+                //todo: remains to be seen Bukkit.getPluginManager().disablePlugin(plugin);
+            }
         }
     }
 
@@ -42,32 +52,43 @@ public class NMSManager {
     }
 
     private static String getServerVersion() {
-        if (Bukkit.getServer().getClass().getPackage().getName().contains("_R"))
-            return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-        if (!Bukkit.getServer().getVersion().contains("-")) {
-            pluginProvider.getLogger().warning("Incompatible Minecraft version detected! [1] Package: " + Bukkit.getServer().getClass().getPackage().getName() + " version: " + Bukkit.getServer().getVersion() + " ! Report this to the developer.");
+        String packageName = Bukkit.getServer().getClass().getPackage().getName();
+        if (packageName.contains("_R")) {
+            return packageName.split("\\.")[3];
+        }
+
+        String versionString = Bukkit.getServer().getVersion();
+        if (!versionString.contains("-")) {
+            pluginProvider.getLogger().warning("Incompatible Minecraft version detected! [1] Package: " + packageName + " version: " + versionString + " ! Report this to the developer.");
             return null;
         }
 
-        try {//Format: X.XX.XX-YYY-ZZZZZZZ where X is the actual version
-            String justVersion = Bukkit.getServer().getVersion().split("-")[0];
+        try {
+            String justVersion = versionString.split("-")[0];
             int major = Integer.parseInt(justVersion.split("\\.")[1]);
-            int minor = Integer.parseInt(justVersion.split("\\.")[2]);
+            int minor;
+            try {
+                minor = Integer.parseInt(justVersion.split("\\.")[2]);
+            } catch (Exception e) {
+                minor = 0;
+            }
             return getInternalsFromRevision(major, minor);
-        } catch (Exception e){
-            pluginProvider.getLogger().warning("Incompatible Minecraft version detected! [2] Package: " + Bukkit.getServer().getClass().getPackage().getName() + " version: " + Bukkit.getServer().getVersion() + " ! Report this to the developer.");
+        } catch (Exception e) {
+            pluginProvider.getLogger().warning("Incompatible Minecraft version detected! [2] Package: " + packageName + " version: " + versionString + " ! Report this to the developer.");
             return null;
         }
-        //paper doing paper things
     }
 
-    //Paper decided to change internal versioning so this needs to get updated from 1.20.6 onward with mapping the version to the correct module
-    private static String getInternalsFromRevision(int major, int minor){
+    private static String getInternalsFromRevision(int major, int minor) {
         String versionString = "v1_";
-        if (major == 20){
+        if (major == 20) {
             versionString += "20_";
             if (minor == 6)
                 return versionString + "R4";
+        } else if (major == 21) {
+            versionString += "21_";
+            if (minor == 0)
+                return versionString + "R1";
         }
         pluginProvider.getLogger().warning("Incompatible Minecraft version detected! [3] Package: " + Bukkit.getServer().getClass().getPackage().getName() + " version: " + Bukkit.getServer().getVersion() + " ! Report this to the developer.");
         return null;
---
+++ EasyMinecraftGoals/v1_21_R1/src/main/java/com/magmaguy/easyminecraftgoals/v1_21_R1/packets/PacketArmorStandEntity.java
@@ -0,0 +1,96 @@
+package com.magmaguy.easyminecraftgoals.v1_21_R1.packets;
+
+import com.magmaguy.easyminecraftgoals.internal.PacketModelEntity;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.Rotations;
+import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_21_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.util.EulerAngle;
+
+import java.util.List;
+import java.util.UUID;
+
+public class PacketArmorStandEntity extends AbstractPacketEntity<ArmorStand> implements PacketModelEntity {
+
+    private ItemStack nmsLeatherHorseArmor;
+    private org.bukkit.inventory.ItemStack leatherHorseArmor;
+    private ArmorStand armorStand;
+
+    public PacketArmorStandEntity(Location location) {
+        super(location);
+    }
+
+    @Override
+    protected ArmorStand createEntity(Location location) {
+        //This doesn't create a real entity until it gets added to the world, which for packet entity purposes is never
+        return new ArmorStand(EntityType.ARMOR_STAND, getNMSLevel(location));
+    }
+
+    public void initializeModel(Location location, int modelID) {
+        armorStand = entity;
+        armorStand.setInvisible(true);
+        armorStand.setMarker(true);
+        leatherHorseArmor = new org.bukkit.inventory.ItemStack(Material.LEATHER_HORSE_ARMOR);
+        LeatherArmorMeta itemMeta = (LeatherArmorMeta) leatherHorseArmor.getItemMeta();
+        itemMeta.setCustomModelData(modelID);
+        itemMeta.setColor(Color.WHITE);
+        leatherHorseArmor.setItemMeta(itemMeta);
+
+        nmsLeatherHorseArmor = CraftItemStack.asNMSCopy(leatherHorseArmor);
+        //Actually useless, managed by the packet that sends equipment info
+        armorStand.setItemSlot(EquipmentSlot.HEAD, nmsLeatherHorseArmor);
+    }
+
+    @Override
+    public void setScale(float scale) {
+        //Actually not possible for armor stands, sorry
+    }
+
+    @Override
+    public void setHorseLeatherArmorColor(Color color) {
+        LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) leatherHorseArmor.getItemMeta();
+        leatherArmorMeta.setColor(color);
+        leatherHorseArmor.setItemMeta(leatherArmorMeta);
+        nmsLeatherHorseArmor = CraftItemStack.asNMSCopy(leatherHorseArmor);
+        armorStand.setItemSlot(EquipmentSlot.HEAD, nmsLeatherHorseArmor);
+    }
+
+    @Override
+    public void sendLocationAndRotationPacket(Location location, EulerAngle eulerAngle) {
+        teleport(location);
+        rotate(eulerAngle);
+    }
+
+    @Override
+    public void displayTo(Player player) {
+        super.displayTo(player);
+        sendPacket(player, new ClientboundSetEquipmentPacket(entity.getId(), List.of(Pair.of(EquipmentSlot.HEAD, nmsLeatherHorseArmor))));
+    }
+
+    public void displayTo(UUID player) {
+        displayTo(Bukkit.getPlayer(player));
+    }
+
+    @Override
+    public void addViewer(UUID player) {
+        super.addViewer(player);
+        displayTo(player);
+    }
+
+    private void rotate(EulerAngle eulerAngle) {
+        if (eulerAngle == null) return;
+        entity.setHeadPose(new Rotations((float) Math.toDegrees(eulerAngle.getX()), (float) Math.toDegrees(eulerAngle.getY()), (float) Math.toDegrees(eulerAngle.getZ())));
+        sendPacket(createEntityDataPacket());
+    }
+
+}
