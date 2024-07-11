2419989e218328fa2d3c297e7dfcbba4aa56bce4
Mitchell Gertrude <mitchgertrude222@gmail.com>
2024-07-11 12:58:57 +1000

    Update v1_21_R1 Hitbox
    Fixed vR1_21_R1 hitbox to set entity's hitbox field to class AABB instead of EntityDimensions

--- EasyMinecraftGoals/v1_21_R1/src/main/java/com/magmaguy/easyminecraftgoals/v1_21_R1/hitbox/Hitbox.java
+++ EasyMinecraftGoals/v1_21_R1/src/main/java/com/magmaguy/easyminecraftgoals/v1_21_R1/hitbox/Hitbox.java
@@ -2,6 +2,7 @@ package com.magmaguy.easyminecraftgoals.v1_21_R1.hitbox;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.phys.AABB;
 
 import java.lang.reflect.Field;
 
@@ -11,11 +12,25 @@ public class Hitbox {
 
     public static boolean setCustomHitbox(Entity entity, float width, float height, boolean fixed) {
         EntityDimensions entityDimensions = new EntityDimensions(width, height, height, null, fixed);
+
+        double x = entity.getX();
+        double y = entity.getY();
+        double z = entity.getZ();
+
+        // Calculate the half dimensions
+        double halfWidth = entityDimensions.width() / 2.0;
+        double dHeight = entityDimensions.height();
+
+        AABB boundingBox = new AABB(
+                x - halfWidth, y, z - halfWidth,  // min corner
+                x + halfWidth, y + dHeight, z + halfWidth  // max corner
+        );
+
         Class<?> entityClass = Entity.class;
         try {
             Field field = entityClass.getDeclaredField("aF");
             field.setAccessible(true);
-            field.set(entity, entityDimensions);
+            field.set(entity, boundingBox);
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
             return false;
